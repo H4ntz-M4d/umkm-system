795c1bd772efa3ea4d8bd755651bba7d936eb603
H4ntz-M4d <muhadiahmad71@gmail.com>
2026-04-14 01:44:13 +0700

    fixing ts and eslint bug

--- apps/backend-system/src/common/decorator/auth.decorator.ts
+++ apps/backend-system/src/common/decorator/auth.decorator.ts
@@ -1,14 +1,24 @@
-import { createParamDecorator, ExecutionContext, HttpException } from "@nestjs/common";
-import e from "express";
+import {
+  createParamDecorator,
+  ExecutionContext,
+  HttpException,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+export interface JwtPayload {
+  sub: bigint;
+  username: string;
+  role: string;
+}
 
 export const AuthUser = createParamDecorator(
-    (data: unknown, context: ExecutionContext) => {
-        const req = context.switchToHttp().getRequest()
-        const user = req.user
-        if (user) {
-            return user
-        } else {
-            throw new HttpException('Unauthorized', 401)
-        }
+  (data: unknown, context: ExecutionContext) => {
+    const req = context.switchToHttp().getRequest<Request>();
+    const user = req.user as JwtPayload;
+    if (user) {
+      return user;
+    } else {
+      throw new HttpException('Unauthorized', 401);
     }
-)
+  },
+);
